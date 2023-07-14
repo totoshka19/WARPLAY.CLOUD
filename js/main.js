6c46db86877f7e1d358f1edb7135b2fe64651524
Anna Kapitanova <totoshechka19@gmail.com>
2023-07-14 19:33:29 +0300

    Убрала лишние пробелы и закомментированные строки.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,7 @@
 const menuBtn = document.querySelector(".menu-btn");
-//const menuBtnClose = document.querySelector(".menu-btn-close");
 const menuMobile = document.querySelector(".menu-mobile");
 
 menuBtn.addEventListener('click', () => {
     menuMobile.classList.toggle('open');
     menuBtn.classList.toggle('active');
-    // menuBtn.style.zIndex = '30';
-
 });
-
-// menuBtnClose.addEventListener('click', () => {
-//     menuMobile.classList.remove('open');
-//     menuBtnClose.style.zIndex = '10'
-//
-// });
-
-
-// menuBtn.onclick = function () {
-//     alert("Вы нажали на кнопку");
-// }
