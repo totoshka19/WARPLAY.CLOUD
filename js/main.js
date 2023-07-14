f4356ad5dea6a31db24a7efb8bd1401bf7cd9b1e
Anna Kapitanova <totoshechka19@gmail.com>
2023-07-14 20:34:02 +0300

    Сделала установку активной ссылки в навигации

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Показ/скрытие мобильного меню
 const menuBtn = document.querySelector(".menu-btn");
 const menuMobile = document.querySelector(".menu-mobile");
 
@@ -5,3 +6,19 @@ menuBtn.addEventListener('click', () => {
     menuMobile.classList.toggle('open');
     menuBtn.classList.toggle('active');
 });
+
+// Установка активной ссылки в навигации
+const navLinks = document.querySelectorAll(".nav-link");
+
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', () => {
+        removeActiveLink();
+        navLinks[i].classList.add('nav-link-active');
+    })
+}
+
+function removeActiveLink() {
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].classList.remove('nav-link-active');
+    }
+}
