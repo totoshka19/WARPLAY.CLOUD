dd7431c93cd156bc1dec740159f227a532b76017
Anna Kapitanova <totoshechka19@gmail.com>
2023-07-16 22:50:22 +0300

    Добавила js в секцию тарифы

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener('click', () => {
         removeActiveLink();
         navLinks[i].classList.add('nav-link-active');
-    })
+    });
 }
 
 function removeActiveLink() {
@@ -24,6 +24,33 @@ function removeActiveLink() {
 }
 
 // Установка активного тарифа
+const tariffBtns = document.querySelectorAll(".btn-tariff");
+const tariffItems = document.querySelectorAll(".tariff-item");
+const tariffTitle = document.querySelectorAll(".tariff-title");
+const tariffPrices = document.querySelectorAll(".tariff-price");
+const tariffLists = document.querySelectorAll(".tariff-list");
+
+for (let i = 0; i < tariffBtns.length; i++) {
+    tariffBtns[i].addEventListener('click', () => {
+        removeActiveStyle();
+        tariffBtns[i].classList.add('active');
+        tariffItems[i].classList.add('active');
+        tariffTitle[i].classList.add('active');
+        tariffPrices[i].classList.add('active');
+        tariffLists[i].classList.add('active');
+    });
+}
+
+function removeActiveStyle() {
+    for (let i = 0; i < tariffBtns.length; i++) {
+        tariffBtns[i].classList.remove('active');
+        tariffItems[i].classList.remove('active');
+        tariffTitle[i].classList.remove('active');
+        tariffPrices[i].classList.remove('active');
+        tariffLists[i].classList.remove('active');
+    }
+}
+
 
 
 
